dfeb3c49b3e393077c3e7f6faf83c5db86918535
Kimsol <ksol92789@gmail.com>
2021-04-11 19:15:55 +0900

    First Commit
    Upload Unity File

---
+++ 1_basic_class/Assets/Scripts/Chick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chick : MonoBehaviour
+{
+    //[SerializeField]
+    private float moveSpeed = 23.0f;
+    private float destroyedTime = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, destroyedTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();          
+    }
+
+    private void Move()
+    {
+        this.transform.Translate(new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ 1_basic_class/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text chickText;
+    private Rigidbody rigid;
+    public GameObject chickPrefab;
+    public GameObject treePrefab;
+    private Vector3 myFirstPosition;
+    private Vector3 moveVector;
+
+    private float moveSpeed = 15.0f;
+    private float chickMakingTime = 1.0f;
+    private float treeMakingTime = 3.0f;
+    private bool isMakingChick = false;
+    private bool isMakingTree = false;
+    public int chickCount = 0;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        myFirstPosition = this.gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+        if(isMakingChick == false)
+            StartCoroutine("CreateNewChicks");
+        if (isMakingTree == false)
+            StartCoroutine("CreateNewTree");
+    }
+
+    private void Move()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector = moveVector.x * Vector3.right;
+        this.transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator CreateNewChicks()
+    {
+        isMakingChick = true;
+        Instantiate(chickPrefab, new Vector3(myFirstPosition.x + Random.Range(-9f, 9f), 0, -100), Quaternion.identity);
+        yield return new WaitForSeconds(chickMakingTime);    
+        isMakingChick = false;
+    }
+
+    IEnumerator CreateNewTree()
+    {
+        isMakingTree = true;
+        Instantiate(treePrefab, new Vector3(0,0,-100), Quaternion.identity);
+        yield return new WaitForSeconds(treeMakingTime);
+        isMakingTree = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Chick"))
+        {            
+            chickCount += 1;         
+            this.chickText.text = chickCount.ToString();
+            Destroy(collision.gameObject, 0);
+        }
+    }
+}
+
